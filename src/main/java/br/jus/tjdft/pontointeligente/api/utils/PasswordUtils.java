4f39e741fbf389e2714ea0c393817b73d93c22b0
Ivaldo Nascimento Oliveira <ivaldo.oliveira@gmail.com>
2019-04-07 17:38:07 -0300

    Repositorio de Empresa, Funcionario e Lancamento Servico de Empresa Testes unitarios das classes de repositorio e de servico.

---
+++ src/main/java/br/jus/tjdft/pontointeligente/api/utils/PasswordUtils.java
@@ -0,0 +1,25 @@
+package br.jus.tjdft.pontointeligente.api.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+	
+	private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+	
+	public PasswordUtils() {
+	}
+	
+	public static String gerarBCrypt(String senha) {
+		if (senha == null) {
+			return senha;
+		}
+		
+		log.info("Gerando hash com o BCrypt");
+		BCryptPasswordEncoder bCryptPasswordEncripter = new BCryptPasswordEncoder();
+		return bCryptPasswordEncripter.encode(senha);
+	}
+	
+	
+}
---
+++ src/test/java/br/jus/tjdft/pontointeligente/api/repositories/LancamentoRepositoryTest.java
@@ -0,0 +1,103 @@
+package br.jus.tjdft.pontointeligente.api.repositories;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.jus.tjdft.pontointeligente.api.entities.Empresa;
+import br.jus.tjdft.pontointeligente.api.entities.Funcionario;
+import br.jus.tjdft.pontointeligente.api.entities.Lancamento;
+import br.jus.tjdft.pontointeligente.api.enums.PerfilEnum;
+import br.jus.tjdft.pontointeligente.api.enums.TipoEnum;
+import br.jus.tjdft.pontointeligente.api.utils.PasswordUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class LancamentoRepositoryTest {
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	private Long funcionarioId;
+	
+	private static String EMAIL = "email@email.com";
+
+	private static String CPF = "24291173474";
+	
+	@Before
+	public void setUp() throws Exception {
+		Empresa empresa = this.empresaRepository.save(obterDadosEmpresa());
+		
+		Funcionario funcionario = this.funcionarioRepository.save(obterDadosFuncionario(empresa));
+		this.funcionarioId = funcionario.getId();
+		
+		this.lancamentoRepository.save(obterDadosLancamentos(funcionario));
+		this.lancamentoRepository.save(obterDadosLancamentos(funcionario));
+	}
+
+	@After
+	public final void tearDown() {
+		this.lancamentoRepository.deleteAll();
+	}
+	
+	@Test
+	public void testBuscarLancamentosPorFuncionarioId() {
+		List<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(this.funcionarioId);
+		assertEquals(2, lancamentos.size());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testBuscarLancamentoPorFuncionarioIdPaginado() {
+		PageRequest page = new PageRequest(0, 10);
+		Page<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(this.funcionarioId, page);
+		assertEquals(2,  lancamentos.getTotalElements());
+	}
+	
+	private Lancamento obterDadosLancamentos(Funcionario funcionario) {
+		Lancamento lancamento = new Lancamento();
+		lancamento.setData(new Date());
+		lancamento.setTipo(TipoEnum.INICIO_ALMOCO);
+		lancamento.setFuncionario(funcionario);
+		return lancamento;
+	}
+
+	private Funcionario obterDadosFuncionario(Empresa empresa) {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome("Fulano de Tal");
+		funcionario.setPerfil(PerfilEnum.ROLE_USUARIO);
+		funcionario.setSenha(PasswordUtils.gerarBCrypt("123456"));
+		funcionario.setCpf(CPF);
+		funcionario.setEmail(EMAIL);
+		funcionario.setEmpresa(empresa);
+		return funcionario;
+	}
+	
+	private Empresa obterDadosEmpresa() {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de exemplo");
+		empresa.setCnpj("51463645000100");
+		return empresa;
+	}
+	
+	
+}
